a8f61528cdf1d250ba5db0a9913af476703f98e3
Sammy Algharazi <sainfo740@gmail.com>
2025-05-23 17:35:21 -0400

    added sign in ang sign up pages with globe BG

---
+++ app/vanta-bg.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+VANTA.GLOBE({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
+});
