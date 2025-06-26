2fb561f65fb6b38e0a5cbe303de8a1004ac59654
SamWammy <sainfo740@gmail.com>
2025-06-26 14:42:25 -0400

    postgres db is now fully functional with tables, going to work on card styling tommorow

--- app/home/page.js
+++ app/home/page.js
@@ -9,7 +9,7 @@ import vercelIcon from "../../public/vercel-icon.png"
 import Image from 'next/image'
 import { UserProfile } from "@clerk/nextjs";
 import { SignedIn,UserButton } from "@clerk/nextjs";
-
+import {supabase} from "../utils/supabase/client"
 
 
 export default function Page() {
@@ -131,4 +131,12 @@ export default function Page() {
     </div>
   );
   
+}
+
+export function Card({projName,projTechStack,projFocus}){
+  return (
+    <div>className={styles.Card} 
+    <h1> {projName} </h1>
+    </div>
+  )
 }
