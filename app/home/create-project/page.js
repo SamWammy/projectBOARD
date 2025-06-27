f3bf1d097cb175d2b63480c5079f833e36bfa1c6
exploadingappendix <dhuang44@buffalo.edu>
2025-06-26 22:21:18 -0400

    nav bar set up on create project page

--- app/home/create-project/page.js
+++ app/home/create-project/page.js
@@ -1,12 +1,24 @@
-"use client"
+"use client";
+import next from "next";
 import styles from "./style.module.css";
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
+import Image from 'next/image'; 
+import logo from "../../../public/logo.svg";
 
 export default function CreateProj() {
-    return(
-        <div className={styles.frame}>
-            <div className={styles.navbar}></div>
-            <div className={styles.formBack}></div>
+  return (
+    <div className={styles.frame}>
+      <div className={styles.navbar}>
+        <Image src={logo} className={styles.logo} alt="logo" ></Image>
+       
+        <div className={styles.navsection}>
+          <div className={styles.nav}>option</div>
+          <div className={styles.nav}>option</div>
+          <div className={styles.nav}>option</div>
         </div>
-    );
+      </div>
+
+      <div className={styles.formBack}></div>
+    </div>
+  );
 }
